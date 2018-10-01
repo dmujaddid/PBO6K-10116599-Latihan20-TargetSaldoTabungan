2d288f75acd1c56e4e744af93e836f40e1d8edf4
Roby <robyeka@email.unikom.ac.id>
2018-10-01 22:32:05 +0700

    first commit

---
+++ src/pbo6k/pkg10116599/latihan20/targetsaldotabungan/PBO6K10116599Latihan20TargetSaldoTabungan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan20.targetsaldotabungan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program perhitungan lama tabungan sampai mencapai saldo target.
+ * 
+ */
+public class PBO6K10116599Latihan20TargetSaldoTabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
